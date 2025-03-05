0f4e89abd7e443a5063403251f9f32006d1c92ac
Zia <zia_gt@yahoo.com>
2025-03-05 20:06:41 +0100

    check db

--- index.js
+++ index.js
@@ -15,6 +15,18 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
+const mongoose = require("mongoose");
+
+app.get("/check-db", async (req, res) => {
+  try {
+    const collections = await mongoose.connection.db.listCollections().toArray();
+    res.json({ message: "✅ MongoDB connected", collections });
+  } catch (error) {
+    res.status(500).json({ message: "❌ MongoDB connection error", error });
+  }
+});
+
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
