1ad3d2ac86c578ffad468819ce9faa89411efb7e
Zia <zia_gt@yahoo.com>
2025-03-05 02:17:48 +0100

    style.css

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-const express = require('express')
+const express = require("express");
 const path = require("path");
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const cors = require("cors");
+require("dotenv").config();
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+const app = express();
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, "public"))); // Correct way to serve static files
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
-
-
-
-
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Your app is listening on port ${port}`);
+});
